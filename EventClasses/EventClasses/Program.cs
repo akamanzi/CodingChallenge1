d69cb288653d891629d63049a442904aee838b22
Arnold <kamstan6@gmail.com>
2023-12-11 22:29:57 -0500

    Added a class to raise and handle an event when the counter reaches 5

---
+++ EventClasses/EventClasses/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+ class EventsClass
+{
+    public static void Main()
+    {
+        var numberFiveCounter = new CountToFive();
+        numberFiveCounter.numberFiveReached += HandleEvent;
+        numberFiveCounter.OnnumberFiveReached();
+    }
+
+    static void HandleEvent(object sender, EventArgs e)
+    {
+        Console.WriteLine("Counter has reached to number 5!!!!");
+    }
+}
+class CountToFive
+{
+    public event EventHandler numberFiveReached;
+    
+    public void OnnumberFiveReached()
+    {
+        for(int i=0; i <= 10; i++)
+        {
+            if(i == 5)
+            {
+                numberFiveReached?.Invoke(this, EventArgs.Empty);
+            }
+        }
+        
+    }
+
+
+}
